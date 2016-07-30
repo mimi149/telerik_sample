07bd37ad32aacadf9c4522bc3ce610a156eaf9ba
Tran Nguyen <baotranmimi@gmail.com>
2016-07-30 23:50:17 +0000

    Finish step 7: Add "fade" or "zoom" transition. Changes made by Tran Nguyen (baotranmimi@gmail.com).

--- app/js/app.js
+++ app/js/app.js
@@ -22,11 +22,11 @@
 
     document.addEventListener("deviceready", function () {
         navigator.splashscreen.hide();
-
         app = new kendo.mobile.Application(document.body, {
-            layout: "main-layout"
+            layout: "main-layout",
+            // 
+            transition: "zoom"
         });
-
     }, false);
 
     window.app = app;
